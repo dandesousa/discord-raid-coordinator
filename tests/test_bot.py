80e3411482f6e82addeb8f1bd7ade1fd1b084e6d
Daniel DeSousa <github@daniel.desousa.cc>
2017-07-29 12:16:47 -0400

    shippable comparison

---
+++ tests/test_bot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class TestFoo(unittest.TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_name(self):
+        pass
