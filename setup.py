f27e0d82f8ac43a69f422a83b2e506de13976ece
Daniel DeSousa <github@daniel.desousa.cc>
2017-08-05 07:03:51 -0400

    ability to create a release w/ better classifiers

--- setup.py
+++ setup.py
@@ -12,13 +12,19 @@ setup(name='discord-raid-coordinator',
       version='0.1.0',
       author='Daniel DeSousa',
       author_email='discord-raid-coordinator@daniel.desousa.cc',
+      license='UNLICENSE',
       packages=find_packages("src"),
-      package_dir={"": "src"},
+      package_dir={"": "src", "": ["LICENSE"]},
       include_package_data=True,
       install_requires=[
         'discord.py>=0.16.8',
         'pytz>=2017.2',
       ],
+      platforms='any',
       test_suite='setup.my_test_suite',
-      entry_points={'console_scripts': ['raid_coordinator = raid_coordinator.bot:main']}
-      )
+      entry_points={'console_scripts': ['raid_coordinator = raid_coordinator.bot:main']},
+      classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
+)
