7546c2fc21b6497d6f669b74f96793f549b8b9dc
Daniel DeSousa <github@daniel.desousa.cc>
2018-03-04 15:48:37 -0500

    restricting verisons

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='discord-raid-coordinator',
       package_dir={"": "src"},
       include_package_data=True,
       install_requires=[
-        'discord.py>=0.16.8',
+        'discord.py>=0.16.8,<0.16.9',
         'pytz>=2017.2',
       ],
       extras_require={
